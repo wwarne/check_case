535e886b29989c07933cab8fd1c586dfec91001e
Dmitry Plevkov <wwarne@gmail.com>
2016-07-25 14:23:39 +0300

    ADD check that day and month from request are inside correct intervals.

--- server.py
+++ server.py
@@ -11,8 +11,13 @@ import msgpack
 from aiohttp import web
 from msgpack.exceptions import UnpackException
 
-# Регулярное выражение, которым будем проверять входящие данные
-CHECKER = re.compile(r'[0-9]{2}.[0-9]{2}.[0-9]{4}')
+"""
+Регулярное выражение, которым будем проверять входящие данные.
+т.к. в условии сказано, что это дата в формате день, месяц, год, я решил проверять также и правильность даты.
+иначе можно подать серверу дату в формате 12.31.2016 (месяц, день, год) и он скажет, что всё ок.
+И на 99.99.9999 тоже сказал бы, что всё ок.
+"""
+CHECKER = re.compile(r'(?P<day>[0-9]{2}).(?P<month>[0-9]{2}).[0-9]{4}')
 # И сразу сделаем две заготовки для ответов, чтобы каждый запрос не вызывать msgpack
 ANSWER_OK = msgpack.packb({'result': 'ok'})
 ANSWER_ERR = msgpack.packb({'result': 'error'})
@@ -26,10 +31,12 @@ async def handle_post(request):
     except UnpackException:
         return web.HTTPBadRequest()
     # проверяем правильность поля test_date и отсылаем нужный ответ
-    if CHECKER.search(data['test_date']):
-        return web.Response(body=ANSWER_OK)
-    else:
-        return web.Response(body=ANSWER_ERR)
+    check_result = CHECKER.search(data['test_date'])
+    if check_result:
+        # теперь дополнительно проверим, чтобы день был не больше 31, а месяц - не больше 12
+        if int(check_result.group('day')) <= 31 and int(check_result.group('month')) <= 12:
+            return web.Response(body=ANSWER_OK)
+    return web.Response(body=ANSWER_ERR)
 
 # Создаём сервер
 app = web.Application()
